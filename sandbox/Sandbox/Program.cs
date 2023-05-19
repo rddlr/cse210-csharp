d8c860cfa04875b54ea000a21560eb89a776041a
rddlr <meoderyan1@gmail.com>
2023-05-19 21:20:32 +0800

    try sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string response = "yes";
+
+        while (response.ToLower() == "yes")
+        {
+            Console.Write("Write your grade percentage: ");
+            string answer = Console.ReadLine();
+            int percent = int.Parse(answer);
+
+            string letter = "";
+
+            if (percent >= 90)
+            {
+                letter = "A";
+            }
+            else if (percent >= 80)
+            {
+                letter = "B";
+            }
+            else if (percent >= 70)
+            {
+                letter = "C";
+            }
+            else if (percent >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+            Console.WriteLine($"Your grade is: {letter}");
+
+            if (percent >= 70)
+            {
+                Console.WriteLine("You passed!");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+            
+            Console.WriteLine("Do you want to try again? ");
+            response = Console.ReadLine();
+        }
     }
 }
